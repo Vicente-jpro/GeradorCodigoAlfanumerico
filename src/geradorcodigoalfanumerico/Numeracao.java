a7a0cd8a066f9a73491644e3565689b4f5683943
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-02-14 19:42:41 +0100

    New class added

---
+++ src/geradorcodigoalfanumerico/Numeracao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package geradorcodigoalfanumerico;
+
+/**
+ *
+ * @author vicente-simao
+ */
+public class Numeracao {
+    
+    private int[] numeros = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    
+}
